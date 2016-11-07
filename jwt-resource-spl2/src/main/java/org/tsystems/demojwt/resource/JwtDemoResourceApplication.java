44d6f0840f2f1334fca7f444deb6d3fac5e3acb3
jubenite <juanantonio.benitez@gmail.com>
2016-11-07 22:52:20 +0100

    Initial Commit

---
+++ jwt-resource-spl2/src/main/java/org/tsystems/demojwt/resource/JwtDemoResourceApplication.java
@@ -0,0 +1,12 @@
+package org.tsystems.demojwt.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtDemoResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtDemoResourceApplication.class, args);
+    }
+}
